0f41d10a2cb1282e6aea4ff37f962127bfab9bef
erfan <e.sobhaei@gmail.com>
2020-05-01 17:47:07 +0430

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'ui/movie_list.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(title: Text("iMovie"),),
+        body: MovieList(),
+      ),
+    );
+  }
+}
---
+++ lib/src/ui/movie_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MovieList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
