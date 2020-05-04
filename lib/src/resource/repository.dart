49a8b1903922f7225f4a0e73d438206466233669
erfan <e.sobhaei@gmail.com>
2020-05-04 18:44:51 +0430

    Repository class is able to store TrailerModel objects

--- lib/src/resource/repository.dart
+++ lib/src/resource/repository.dart
@@ -1,8 +1,10 @@
 import 'package:tmdbclient/src/model/item_model.dart';
+import 'package:tmdbclient/src/model/trailer_model.dart';
 import 'package:tmdbclient/src/resource/movie_api_provider.dart';
 
 class Repository{
   final MovieApiProvider provider = MovieApiProvider();
 
   Future<ItemModel> fetchAllMovies() => provider.fetchMovieList();
+  Future<TrailerModel> fetchTrailer(int movieId) => provider.fetchTrailer(movieId);
 }
